c8896ea8964354eb814c2c77cb355f97109c299b
kngurley <kngurley25@gmail.com>
2022-01-24 18:49:56 -0600

    update idb for transaction

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,7 +10,7 @@ request.onupgradeneeded = function(event) {
 request.onsuccess = function(event) {
     db = event.target.result;
     if (navigator.onLine) {
-        // upload the transaction
+        uploadTransaction()
     }
 };
 
@@ -23,3 +23,36 @@ function saveRecord(record) {
     const transactionObjectStore = transaction.objectStore('new_transaction');
     transactionObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+    const getAll = transactionObjectStore.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                  Accept: "application/json, text/plain, */*",
+                  "Content-Type": "application/json"
+                }
+            })
+            .then(response => {    
+                response.json();
+            })
+            .then(serverResponse => {
+                const transaction = db.transaction(['new_transaction'], 'readwrite');
+                const transactionObjectStore = transaction.objectStore('new_transaction');
+                transactionObjectStore.clear();
+                alert('All saved transactions have been submitted');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
+};
+
+window.addEventListener('online', uploadTransaction);
